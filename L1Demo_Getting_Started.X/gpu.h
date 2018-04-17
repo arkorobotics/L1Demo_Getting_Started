5c7069b12ea28f40495a7c7bde4b3ef0c5001e86
Arko <dotuplink@gmail.com>
2018-04-16 21:24:21 -0700

    Added alternative resolutions
    Added alternative resolutions. Thanks Jamis!

--- L1Demo_Getting_Started.X/gpu.h
+++ L1Demo_Getting_Started.X/gpu.h
@@ -13,6 +13,94 @@
 #define VER_PULSE_WIDTH 5
 #define VER_BACK_PORCH  10
 
+// #ifdef DOUBLE_BUFFERED
+// //80x480@60: 8bpp
+// #define CLOCKDIV 69
+// #define HOR_RES 80UL
+// #define VER_RES 480UL
+// #define HOR_FRONT_PORCH 32
+// #define HOR_PULSE_WIDTH 16
+// #define HOR_BACK_PORCH  32
+// #define VER_FRONT_PORCH 10
+// #define VER_PULSE_WIDTH 5
+// #define VER_BACK_PORCH  10
+// #define BPP 8
+
+// #else
+// //80x480@60: 16bpp
+// #define CLOCKDIV 69
+// #define HOR_RES 80UL
+// #define VER_RES 480UL
+// #define HOR_FRONT_PORCH 32
+// #define HOR_PULSE_WIDTH 16
+// #define HOR_BACK_PORCH  32
+// #define VER_FRONT_PORCH 10
+// #define VER_PULSE_WIDTH 5
+// #define VER_BACK_PORCH  10
+// #define BPP 16
+// #endif
+
+// 160x480@4bpp
+//#define CLOCKDIV 47
+//#define HOR_RES 160UL
+//#define VER_RES 480UL
+//#define HOR_FRONT_PORCH 32
+//#define HOR_PULSE_WIDTH 24
+//#define HOR_BACK_PORCH  32
+//#define VER_FRONT_PORCH 10
+//#define VER_PULSE_WIDTH 5
+//#define VER_BACK_PORCH  10
+//#define BPP 8
+
+// 320x480@4bpp
+//#define CLOCKDIV 25
+//#define HOR_RES 320UL
+//#define VER_RES 480UL
+//#define HOR_FRONT_PORCH 32
+//#define HOR_PULSE_WIDTH 48
+//#define HOR_BACK_PORCH  32
+//#define VER_FRONT_PORCH 10
+//#define VER_PULSE_WIDTH 5
+//#define VER_BACK_PORCH  10
+//#define BPP 4
+
+// 480x480@2bpp
+//#define CLOCKDIV 17
+//#define HOR_RES 480UL
+//#define VER_RES 480UL
+//#define HOR_FRONT_PORCH 32
+//#define HOR_PULSE_WIDTH 64
+//#define HOR_BACK_PORCH  32
+//#define VER_FRONT_PORCH 10
+//#define VER_PULSE_WIDTH 5
+//#define VER_BACK_PORCH  10
+//#define BPP 2
+
+//640x480@60: 1bpp
+//#define CLOCKDIV 11
+//#define HOR_RES 640UL
+//#define VER_RES 480UL
+//#define HOR_FRONT_PORCH 16
+//#define HOR_PULSE_WIDTH 96
+//#define HOR_BACK_PORCH  48
+//#define VER_FRONT_PORCH 10
+//#define VER_PULSE_WIDTH 2
+//#define VER_BACK_PORCH  33
+//#define BPP 1
+
+
+// QVGA:
+//#define CLOCKDIV 58
+//#define HOR_RES 320UL
+//#define VER_RES 240UL
+//#define HOR_FRONT_PORCH 32
+//#define HOR_PULSE_WIDTH 16
+//#define HOR_BACK_PORCH  32
+//#define VER_FRONT_PORCH 5
+//#define VER_PULSE_WIDTH 3
+//#define VER_BACK_PORCH  5
+//#define BPP 8
+
 #ifdef DOUBLE_BUFFERED
 #define BPP 2
 #else
